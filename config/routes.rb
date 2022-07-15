3485b1e56c45e8496eb1b6968f8635e1bfdd3d9d
= <=>
2022-07-15 18:54:13 -0400

    definition root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :events
   resources :bands
   get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
