773fe444ef10876daa109dcee3204db3e8662b88
= <=>
2022-07-15 22:00:59 -0400

    dashboard ver 1.0

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def dashboard
+    @bands = Band.order(:name)
+  end
 end
--- app/models/band.rb
+++ app/models/band.rb
@@ -9,4 +9,29 @@ class Band < ApplicationRecord
     def to_s
         self.name
     end
+
+    # mostrar la cantidad de conciertos de cada banda
+    def q_concerts
+        self.concerts.count
+    end
+
+    # mostrar la fecha del primer concierto de cada banda
+
+    def date_debut_dl
+        if self.concerts.present?
+            "#{self.concerts.first.event.name} #{self.concerts.first.event.date_event.strftime("%d/%m/%Y")}"
+        else
+            "--NO DEBUT IN DL--"
+        end
+    end
+
+
+    #  mostrar la cantidad total de asistentes (suma de todos los asistentes a los conciertos de cada banda)
+    def q_total_audience
+        if self.concerts.present?
+            self.concerts.sum(:q_audience)
+        else
+            0
+        end
+    end
 end
